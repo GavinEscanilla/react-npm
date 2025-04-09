c12f371cb7769c849edfd60d35924a959d085477
GavinEscanilla <gavinaltonescanilla@gmail.com>
2025-04-08 22:51:09 -0400

    start

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <div className="App bg-red-900 h-screen">
-      Boiler plate
-    </div>
-  )
+  return <div className="App bg-gray-100 h-screen">
+  
+<h1 className="text-8xl font-bold text-center">Wordle</h1>
+
+  
+  </div>;
 }
 
-export default App
+export default App;
